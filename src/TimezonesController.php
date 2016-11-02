6c5d15db8769947f69509c6caf2aacb5adfaba34
Med aziz lahkimi <lahkimi.maziz@gmail.com>
2016-11-02 15:56:15 +0000

    Applied fixes from StyleCI

--- src/TimezonesController.php
+++ src/TimezonesController.php
@@ -1,14 +1,16 @@
 <?php
+
 namespace Unabomber\Timezone;
 
 use App\Http\Controllers\Controller;
 use Carbon\Carbon;
 
-class TimezonesController extends Controller {
+class TimezonesController extends Controller
+{
+    public function index($timezone)
+    {
+        $datetime = Carbon::now(str_replace('-', '/', $timezone))->toDateTimeString();
 
-	public function index($timezone) {
-		$datetime = Carbon::now(str_replace('-', '/', $timezone))->toDateTimeString();
-		return view('timezones::time', compact('datetime'));
-	}
-
+        return view('timezones::time', compact('datetime'));
+    }
 }
